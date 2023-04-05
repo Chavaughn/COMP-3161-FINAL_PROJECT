a4c08656aa21030d3e35f01f79dbd41ce4fe7fe5
Chavvv <chavaughnwilkins@gmail.com>
2023-04-05 15:03:11 -0500

    Config file edit put you password there

--- app/config.py
+++ app/config.py
@@ -8,5 +8,6 @@ class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('SECRET_KEY', 'HBJHBUGBOIH*(^&vjhsadsa7809)as')
     UPLOAD_FOLDER = os.environ.get('UPLOAD_FOLDER')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://')
+    SQLALCHEMY_DATABASE_URI = "mysql://root:yourpasswordhere@localhost/database_final_project"
+    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed)
